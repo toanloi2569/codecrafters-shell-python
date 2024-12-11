f4a7e4a4e487052da2c2e46d2cb0a54d4f23c18d
lointt3 <LoiNTT3@fpt.com.vn>
2024-12-11 17:18:12 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit 0":
+            sys.exit(0)
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
