3ed2ac2864ac8e0679d34903cea2ee1b8f45210e
lointt3 <LoiNTT3@fpt.com.vn>
2024-12-16 18:20:36 +0700

    codecrafters submit [skip ci]

--- app/processors.py
+++ app/processors.py
@@ -1,5 +1,6 @@
 import sys
 import abc
+import os
 
 
 class Processor(abc.ABC):
@@ -23,11 +24,20 @@ class TypeProcessor(Processor):
         return "type"
 
     def process(self, command):
-        param = command[5:]
-        if param in shell_builtins:
-            print(f"{param} is a shell builtin")
-        else:
-            print(f"{param}: not found")
+        path_list = os.environ.get("PATH")
+        paths = path_list.split(":")
+
+
+        if command[5:] in shell_builtins:
+            print(command[5:] + " is a shell builtin")
+            return
+
+        for path in paths:
+            if os.path.isfile(path + "/" + command[5:]):
+                print(command[5:] + " is " + path + "/" + command[5:])
+                return
+
+        print(f"{command[5:]}: not found")
 
 class ExitProcessor(Processor):
     def builtin_command(self):
--- your_program.sh
+++ your_program.sh
@@ -12,4 +12,7 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
+
+# we need set PATH = "/usr/bin:/usr/local/bin" in runtime session
+#export PATH="/usr/bin:/usr/local/bin:$PATH"
 exec python -u -m app.main "$@"
